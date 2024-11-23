dc257c93df7cfa27c0b88cf3151ad8bf3a9e2f14
blue007fff <blue007fff@gmail.com>
2024-11-23 09:01:35 +0900

    feat: add partition project to FeatureExamples solution

---
+++ Cpp/FeatureExamples/partition/partition.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <iterator>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <string>
+#include <format>
+
+namespace
+{
+    auto PrintSplitLines = []() {std::cout << std::format("{:-<{}}\n", "", 50); };
+}
+
+template <typename T>
+void print(const std::vector<T>& list)
+{
+    // for (T i : list)
+    //     std::cout << i << " ";
+    // std::cout << std::endl;
+
+    std::copy(list.begin(), list.end(),
+        std::ostream_iterator<T>(std::cout, ", "));
+    std::cout << '\n';
+}
+
+void example_partition()
+{
+    PrintSplitLines();
+    std::cout << __FUNCTION__ << std::endl;
+    // pred 가 참인 원소들을 거짓인 원소 앞에 배치.
+    // - pred 에서 당연히 v 값을 수정하면 안됨.
+
+    std::vector<int> v = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    auto itr = std::partition(v.begin(), v.end(), [](int i)
+        { return i % 2 == 0; });
+
+    std::copy(std::begin(v), itr, std::ostream_iterator<int>(std::cout, " "));
+    std::cout << " - ";
+    std::copy(itr, std::end(v), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+
+    std::cout.setf(std::ios_base::boolalpha);
+    std::cout << std::is_partitioned(v.begin(), v.end(), [](int i)
+        { return i % 2 == 0; });
+}
+
+int main()
+{
+    // pred 함수값이 true 인 원소들을 false 인 원소들 앞쪽에 배치.
+    // 유사한 함수로, nth_element() 가 있음.
+    // - partition() 은 값 기준, nth_element() 는 인덱스 기준으로 보면 될듯.
+
+    example_partition();
+    return 0;
+}
